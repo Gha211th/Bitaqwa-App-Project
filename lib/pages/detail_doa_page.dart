7adc67397b9e52c3d9267b4e54d2bcbbc5b9c4ca
ghatraxion <agh.arrafif@gmail.com>
2025-11-04 14:21:27 +0700

    1 more page:)

---
+++ lib/pages/detail_doa_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class detail_doa_page extends StatelessWidget {
+  const detail_doa_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
