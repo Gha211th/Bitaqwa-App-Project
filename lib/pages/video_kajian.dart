5d4b27094ddf6b803e0d41618e226096ae7240ab
ghatraxion <agh.arrafif@gmail.com>
2025-11-04 14:12:39 +0700

    i've just make some file

---
+++ lib/pages/video_kajian.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class video_kajian_page extends StatelessWidget {
+  const video_kajian_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/zakat_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class zakat_page extends StatelessWidget {
+  const zakat_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
