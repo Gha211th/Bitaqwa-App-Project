4bfe14e8db4874c0ceb81b6cde218cfaaef7f50c
ghatraxion <agh.arrafif@gmail.com>
2025-11-06 08:46:49 +0700

    i'm stressing now

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; // Times coutdown
+import 'package:carousel_slider/carousel_slider.dart'; // carousel slider
+import 'package:geolocator/geolocator.dart'; // gps
+import 'package:geocoding/geocoding.dart';
+import 'package:permission_handler/permission_handler.dart'; //izin handler
+import 'package:intl/intl.dart'; // formater number
+import 'package:shared_preferences/shared_preferences.dart'; // cache local
+import 'package:http/http.dart' as http; //ambil data API json
+import 'package:string_similarity/string_similarity.dart'; //fuzzy match string
+import 'dart:convert'; //decode json
 
-class Home_Page extends StatelessWidget {
+class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
 
-  final posterList = const <String>[];
+  @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
+  final CarouselController _controller = CarouselController();
+  final _currentIndex = 0;
+
+  final posterList = const <String>[
+    'assets/images/ramadan.jpg',
+    'assets/images/idul_adha.jpg',
+    'assets/images/idul_fitri.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +39,8 @@ class Home_Page extends StatelessWidget {
                 height: 130,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  color: Colors.green,
-                  border: Border.all(color: Colors.black, width: 2),
+                  color: Colors.yellow[400],
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
@@ -125,9 +147,23 @@ class Home_Page extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCarouselSection() {
+    return Column(
+      children: [
+        SizedBox(width: 10),
+        CarouselSlider.builder(
+          itemCount: posterList.length,
+          itemBuilder: itemBuilder,
+          options: opstions,
+        ),
+      ],
     );
   }
 }
